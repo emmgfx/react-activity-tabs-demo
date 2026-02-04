e80f530225d28613b0cadb5d63ddc2ea3e7bc666
Josep Viciana <emmgfx@gmail.com>
2026-02-04 18:40:40 +0100

    Improve header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,28 @@
 export const Header = () => {
   return (
     <div>
-      <div className="h-16" />
+      <div className="h-8 md:h-16" />
       <div>
-        <div className="flex justify-center text-center items-center capitalize font-semibold text-2xl gap-2">
-          <span className="bg-slate-300 text-slate-700 rounded-t-2xl px-6 py-2">
+        <div className="flex justify-center text-center items-center capitalize font-semibold text-lg md:text-2xl gap-2">
+          <span className="bg-slate-300 text-slate-700 rounded-t-2xl px-3 py-1 md:px-6 md:py-2">
             react
           </span>
           <span className="sr-only">-</span>
-          <span className="bg-slate-300 text-slate-700 rounded-t-2xl px-6 py-2">
+          <span className="bg-slate-300 text-slate-700 rounded-t-2xl px-3 py-1 md:px-6 md:py-2">
             activity
           </span>
           <span className="sr-only">-</span>
-          <span className="bg-slate-600 text-slate-100 rounded-t-2xl px-6 py-2">
+          <span className="bg-slate-600 text-slate-100 rounded-t-2xl px-3 py-1 md:px-6 md:py-2">
             tabs
           </span>
         </div>
       </div>
-      <div className="h-16" />
+      <div className="h-8 md:h-16" />
 
-      <div className="text-center">
+      <div className="text-center text-balance">
         React tabs component that doesn't loose your state
       </div>
-      <div className="h-16" />
+      <div className="h-8 md:h-16" />
     </div>
   );
 };
